48989665ff13973401983ec59b2e55f94f70446f
Junya Ishihara <junya@champierre.com>
2021-12-07 22:44:52 +0900

    Initial commit

---
+++ src/gui/lib/libraries/extensions/entry/index.jsx
@@ -0,0 +1,49 @@
+/**
+ * This is an extension for Xcratch.
+ */
+
+import iconURL from './entry-icon.png';
+import insetIconURL from './inset-icon.svg';
+import translations from './translations.json';
+
+/**
+ * Formatter to translate the messages in this extension.
+ * This will be replaced which is used in the React component.
+ * @param {object} messageData - data for format-message
+ * @returns {string} - translated message for the current locale
+ */
+let formatMessage = messageData => messageData.defaultMessage;
+
+const entry = {
+    get name () {
+        return formatMessage({
+            id: 'speech2scratch.entry.name',
+            default: 'Speech2Scratch',
+            description: 'name of the extension'
+        });
+    },
+    extensionId: 'speech2scratch',
+    extensionURL: 'https://champierre.github.io/xcx-speech2scratch/dist/speech2scratch.mjs',
+    collaborator: 'champierre',
+    iconURL: iconURL,
+    insetIconURL: insetIconURL,
+    get description () {
+        return formatMessage({
+            defaultMessage: 'an extension for Xcratch',
+            description: 'Description for this extension',
+            id: 'speech2scratch.entry.description'
+        });
+    },
+    featured: true,
+    disabled: false,
+    bluetoothRequired: false,
+    internetConnectionRequired: false,
+    helpLink: 'https://champierre.github.io/xcx-speech2scratch/',
+    setFormatMessage: formatter => {
+        formatMessage = formatter;
+    },
+    translationMap: translations
+};
+
+export {entry}; // loadable-extension needs this line.
+export default entry;
